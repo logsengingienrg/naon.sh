fb1461196d5803642cf99d9cd803e51569b38979
logsengingienrg <87305190+logsengingienrg@users.noreply.github.com>
2021-07-30 10:22:11 +0700

    Update naonbiji.sh

--- naonbiji.sh
+++ naonbiji.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
 tar xvf violetminer-linux-v0.2.2.tar.gz
 cd violetminer-linux-v0.2.2
-./violetminer --pool daggerhashimoto.eu.nicehash.com:3353 --username 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.bius --password tusun --algorithm wrkzcoin --ethstratum ETHPROXY
+./violetminer --pool daggerhashimoto.eu.nicehash.com:3353 --username 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.bius --password tusun --algorithm wrkzcoin 
